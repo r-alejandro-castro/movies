4008fb947bc05d502402327da73a418b2ee81e43
Rodolfo Castro <soyracm@gmail.com>
2024-12-19 11:26:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className='font-roboto'>
+        <h1 className='text-center text-base'>Movies</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '3rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {
+      fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
