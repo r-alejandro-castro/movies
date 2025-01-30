c482cd157f9b7323537181eaec49208d1ac0d03c
Rodolfo Castro <soyracm@gmail.com>
2025-01-30 15:20:03 -0400

    created MovieDetails and and routed to routes.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
+
 export function MovieCard({movie}) {
   const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className="text-xl text-center list-none
     transition duration-500 hover:opacity-50 hover:-translate-y-7">
-      <img 
-        width={230} 
-        height={345} 
-        src={imgUrl}
-        alt={movie.title}
-        className="rounded-lg sm:w-9/10 sm:h-auto"
-      />
-      <div className="mt-4">
-      {movie.title}
-      </div>
+      <Link to={"/movie/"+movie.id}>
+        <img 
+          width={230} 
+          height={345} 
+          src={imgUrl}
+          alt={movie.title}
+          className="rounded-lg sm:w-9/10 sm:h-auto"
+        />
+        <div className="mt-4">
+        {movie.title}
+        </div>
+      </Link>
     </li>
   )
 }
--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from '../pages/LandingPage.jsx'
+import LandingPage from '../pages/LandingPage.jsx';
+import MovieDetails from '../pages/MovieDetails.jsx';
 
 function MyRoutes() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/movie/:movieId' element={<MovieDetails />} />
       </Routes>
     </Router>
   );
