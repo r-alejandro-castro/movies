ef434d322626d5725d342a9cc8de33589c83983b
Rodolfo Castro <soyracm@gmail.com>
2025-01-31 15:09:59 -0400

    MovieDetails.jsx and getMovieImg.jx: finishing the logic

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,22 +1,40 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import {get} from '../data/httppClient.jsx';
+import { get } from '../data/httppClient.jsx';
+import { getMovieImg } from '../utils/getMovielmg.jsx';
 
 export default function MovieDetails() {
-
-    const {movieId} = useParams
-    const {movie, setMovie} = useState(null);
+    const { movieId } = useParams(); 
+    const [movie, setMovie] = useState([]);
+    const [generos, setGeneros] = useState([]);
+    
     useEffect(() => {
-        get("/movie/"+movieId).then((data) => {
+        get("/movie/" + movieId).then((data) => {
             setMovie(data);
-        }).catch((error) => {
-            console.error('Error fetching movie details:', error);
-        });
-    }, [movieId, setMovie]);
+            console.log(data)
+            setGeneros(data.genres[0])
+            console.log(data.genres[0])
+        })
+    }, [movieId]);
+
+    const imageUrl = getMovieImg(movie.poster_path, 500);
 
     return (
         <div>
-            <img src="" alt="" />
+            <img src={imageUrl} alt={movie.title} />
+            <div className="">
+                <p>
+                    <strong>Título:</strong> {movie.title}
+                </p>
+                <p>
+                    <strong>Genero : </strong>
+                    {generos.name}
+                </p>
+                <p>
+                    <strong>Descriptions : </strong>
+                    {movie.overview}
+                </p>
+            </div>
         </div>
     );
 }
--- src/utils/getMovielmg.jsx
+++ src/utils/getMovielmg.jsx
@@ -1,7 +1,7 @@
-function getMovieImg(path, width) {
-    return path? `https://image.tmdb.org/t/p/w${width}${path}` : null;
+import placeholder from '../img/placeholder.png';
+
+export function getMovieImg(path, width) {
+    return path? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder
 }
 
 // const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
-
-export default getMovieImg(path, width) ;
